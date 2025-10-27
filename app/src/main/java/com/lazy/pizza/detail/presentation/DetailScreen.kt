a9051cfcad7eec9f26c0a6b7e5ff94ad9a796d9a
Nicolas Marin <nicolasmarin1312@gmail.com>
2025-10-27 15:50:27 -0300

    DetailScreen works for widder screens

--- app/src/main/java/com/lazy/pizza/detail/presentation/DetailScreen.kt
+++ app/src/main/java/com/lazy/pizza/detail/presentation/DetailScreen.kt
@@ -7,19 +7,27 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -28,6 +36,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,10 +50,13 @@ import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.lazy.pizza.R
@@ -73,7 +87,9 @@ import com.lazy.pizza.core.presentation.designsystem.Primary
 import com.lazy.pizza.core.presentation.designsystem.Primary8
 import com.lazy.pizza.core.presentation.designsystem.PrimaryGradientEnd
 import com.lazy.pizza.core.presentation.designsystem.PrimaryGradientStart
+import com.lazy.pizza.core.presentation.designsystem.ScreenConfiguration
 import com.lazy.pizza.core.presentation.designsystem.TextOnPrimary
+import com.lazy.pizza.core.presentation.designsystem.screenConfiguration
 import com.lazy.pizza.detail.presentation.DetailAction.ActionAffectingToppingQuantity.*
 import java.util.Locale
 
@@ -106,171 +122,315 @@ fun DetailScreen(
     state: DetailState,
     onAction: (DetailAction) -> Unit,
     dimens: DimensDetail = MaterialTheme.dimen.detail,
+    screenConfiguration: ScreenConfiguration = MaterialTheme.screenConfiguration
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(BG)
-            .padding(
-                top = statusBarHeight()
-            )
-            .padding(WindowInsets.navigationBars.asPaddingValues())
-    ) {
-        Box(
-            contentAlignment = Alignment.CenterStart,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = dimens.paddingHorizontal,
-                    vertical = dimens.paddingHorizontal
-                )
-        ) {
-            BackButton(
-                onClick = {
-                    onAction(OnBackPressed)
-                }
-            )
-        }
+    val listState = rememberLazyListState()
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(SurfaceHigher)
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        BG,
-                        RoundedCornerShape(
-                            topStart = 0.dp,
-                            topEnd = 0.dp,
-                            bottomStart = 0.dp,
-                            bottomEnd = 16.dp
-                        )
-                    )
-            ) {
-                state.product?.let {
-                    Image(
-                        painter = rememberAsyncImagePainter(Urls.getProductImageUrl(state.product)),
-                        contentDescription = "Icon",
-                        modifier = Modifier
-                            .size(240.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-            }
-        }
+    val modifier = Modifier
+        .fillMaxSize()
+        .background(BG)
+        .padding(
+            top = statusBarHeight()
+        )
+        .padding(WindowInsets.navigationBars.asPaddingValues())
+
+    if (screenConfiguration == ScreenConfiguration.PHONE_PORTRAIT) {
         Box(
             contentAlignment = Alignment.BottomCenter,
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
+            modifier = modifier
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .drawBehind {
-                        val paint = Paint().asFrameworkPaint().apply {
-                            color = "#0A03131F".toColorInt()
-                            setShadowLayer(16f, 0f, -4f, color)
-                        }
-                        drawIntoCanvas {
-                            it.nativeCanvas.drawRect(0f, 0f, size.width, size.height, paint)
-                        }
-                    }
-                    .background(
-                        SurfaceHigher,
-                        RoundedCornerShape(
-                            topStart = 16.dp,
-                            topEnd = 0.dp,
-                            bottomStart = 0.dp,
-                            bottomEnd = 0.dp
-                        )
-                    )
-                    .padding(
-                        start = dimens.paddingHorizontal,
-                        end = dimens.paddingHorizontal,
-                        top = 20.dp,
-                        bottom = 10.dp
-                    )
+            LazyColumn(
+                state = listState,
+                modifier = Modifier.fillMaxSize()
             ) {
-                state.product?.let {
-                    Text(
-                        text = it.name,
-                        style = InstrumentSansSemiBold.copy(
-                            fontSize = 24.sp,
-                            lineHeight = 28.sp
-                        ),
-                        color = TextPrimary
+                item {
+                    CompleteBackButton(
+                        onAction = onAction
                     )
-                    Spacer(Modifier.height(4.dp))
-                    Text(
-                        text = it.ingredients,
-                        style = InstrumentSansRegularNormal.copy(
-                            fontSize = 14.sp,
-                            lineHeight = 18.sp
-                        ),
-                        color = TextSecondary
+                }
+                item {
+                    ProductImage(
+                        state = state,
+                        roundBottomEnd = true,
                     )
-                    Spacer(Modifier.height(16.dp))
-
-                    Text(
-                        text = stringResource(R.string.detail_add_toppings).uppercase(),
-                        style = InstrumentSansSemiBold.copy(
-                            fontSize = 12.sp,
-                            lineHeight = 16.sp
-                        ),
-                        color = TextSecondary
-                    )
-
-                    Spacer(Modifier.height(6.dp))
-
-                    if (!state.isLoading) {
-                        LazyVerticalGrid(
-                            columns = GridCells.Fixed(3),
-                            modifier = Modifier,
-                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                            verticalArrangement = Arrangement.spacedBy(8.dp),
-                        ) {
-                            items(
-                                items = state.toppings,
-                                key = { topping -> topping.id }
-                            ) { topping ->
-                                ToppingCard(
-                                    onAction = onAction,
-                                    topping = topping
-                                )
+                }
+                item {
+                    ToppingsCard(
+                        showNameAndIngredients = true,
+                        state = state,
+                        onAction = onAction,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .drawBehind {
+                                val paint = Paint().asFrameworkPaint().apply {
+                                    color = "#0A03131F".toColorInt()
+                                    setShadowLayer(16f, 0f, -4f, color)
+                                }
+                                drawIntoCanvas {
+                                    it.nativeCanvas.drawRect(0f, 0f, size.width, size.height, paint)
+                                }
                             }
-                            item {
-                                Spacer(Modifier.height(dimens.addCartOverlay))
+                    )
+                }
+            }
+            ButtonWithOverlay(
+                state = state
+            )
+        }
+    } else {
+        Column(
+            modifier = modifier
+        ) {
+            CompleteBackButton(
+                onAction = onAction
+            )
+            Row(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            ) {
+                val density = LocalDensity.current
+                var totalHeight by remember { mutableStateOf(0.dp) }
+                var cartHeight by remember { mutableStateOf(0.dp) }
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .padding(
+                            start = dimens.paddingHorizontal,
+                            end = dimens.paddingHorizontal,
+                            bottom = 10.dp
+                        )
+                        .onGloballyPositioned { coordinates ->
+                            // Get height in pixels
+                            totalHeight = with(density) {
+                                coordinates.size.height.toFloat().toDp()
                             }
                         }
+                ) {
+                    ProductImage(
+                        state = state,
+                        roundBottomEnd = false,
+                    )
+                    state.product?.let {
+                        ProductNameAndIngredients(
+                            product = it
+                        )
+                    }
+                }
+                if (totalHeight != 0.dp) {
+                    Box(
+                        contentAlignment = Alignment.BottomCenter,
+                        modifier = Modifier
+                            .weight(1f)
+                            .heightIn(max = min(cartHeight, totalHeight))
+                    ) {
+                        ToppingsCard(
+                            bottomStart = 16.dp,
+                            showNameAndIngredients = false,
+                            state = state,
+                            onAction = onAction,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .verticalScroll(rememberScrollState())//.heightIn(max = with(density) { totalHeightPx.toDp() } )
+                                .onGloballyPositioned { coordinates ->
+                                    // Get height in pixels
+                                    cartHeight = with(density) {
+                                        coordinates.size.height.toFloat().toDp()
+                                    }
+                                }
+                        )
+                        ButtonWithOverlay(
+                            state = state
+                        )
                     }
                 }
             }
+        }
+    }
+}
 
-            Box(
-                contentAlignment = Alignment.BottomCenter,
-                modifier = Modifier
-                    .height(dimens.addCartOverlay)
-                    .fillMaxWidth()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color(0xFFFFFFFF),          // solid white at start
-                                Color(0xFFFFFFFF).copy(alpha = 0f) // transparent at end
-                            ),
-                            startY = Float.POSITIVE_INFINITY, // 0deg → bottom to top
-                            endY = 0f
+@Composable
+fun CompleteBackButton(
+    onAction: (DetailAction) -> Unit,
+    dimens: DimensDetail = MaterialTheme.dimen.detail,
+) {
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = dimens.paddingHorizontal,
+                vertical = dimens.paddingHorizontal
+            )
+    ) {
+        BackButton(
+            onClick = {
+                onAction(OnBackPressed)
+            }
+        )
+    }
+}
+
+@Composable
+fun ToppingsCard(
+    showNameAndIngredients: Boolean,
+    state: DetailState,
+    onAction: (DetailAction) -> Unit,
+    modifier: Modifier = Modifier,
+    bottomStart: Dp = 0.dp,
+    dimens: DimensDetail = MaterialTheme.dimen.detail,
+) {
+    Column(
+        modifier = modifier
+            .background(
+                SurfaceHigher,
+                RoundedCornerShape(
+                    topStart = 16.dp,
+                    topEnd = 0.dp,
+                    bottomStart = bottomStart,
+                    bottomEnd = 0.dp
+                )
+            )
+            .padding(
+                start = dimens.paddingHorizontal,
+                end = dimens.paddingHorizontal,
+                bottom = 10.dp
+            )
+    ) {
+        state.product?.let {
+            if (showNameAndIngredients) {
+                ProductNameAndIngredients(
+                    product = it
+                )
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = stringResource(R.string.detail_add_toppings).uppercase(),
+                style = InstrumentSansSemiBold.copy(
+                    fontSize = 12.sp,
+                    lineHeight = 16.sp
+                ),
+                color = TextSecondary
+            )
+
+            Spacer(Modifier.height(6.dp))
+
+            if (!state.isLoading) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(3),
+                    modifier = Modifier
+                        .heightIn(max = 1000.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    userScrollEnabled = false // disable inner scroll so outer LazyColumn scrolls
+                ) {
+                    items(
+                        items = state.toppings,
+                        key = { topping -> topping.id }
+                    ) { topping ->
+                        ToppingCard(
+                            onAction = onAction,
+                            topping = topping
                         )
+                    }
+                    item {
+                        Spacer(Modifier.height(dimens.addCartOverlay))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonWithOverlay(
+    state: DetailState,
+    modifier: Modifier = Modifier,
+    dimens: DimensDetail = MaterialTheme.dimen.detail,
+) {
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = modifier
+            .height(dimens.addCartOverlay)
+            .fillMaxWidth()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFFFFFFFF),          // solid white at start
+                        Color(0xFFFFFFFF).copy(alpha = 0f) // transparent at end
+                    ),
+                    startY = Float.POSITIVE_INFINITY, // 0deg → bottom to top
+                    endY = 0f
+                )
+            )
+            .padding(dimens.paddingHorizontal)
+    ) {
+        AddToCartButton(
+            modifier = Modifier.fillMaxWidth(),
+            price = state.cardTotal
+        )
+    }
+}
+
+@Composable
+fun ColumnScope.ProductNameAndIngredients(
+    product: Product
+) {
+    Spacer(Modifier.height(16.dp))
+    Text(
+        text = product.name,
+        style = InstrumentSansSemiBold.copy(
+            fontSize = 24.sp,
+            lineHeight = 28.sp
+        ),
+        color = TextPrimary
+    )
+    Spacer(Modifier.height(4.dp))
+    Text(
+        text = product.ingredients,
+        style = InstrumentSansRegularNormal.copy(
+            fontSize = 14.sp,
+            lineHeight = 18.sp
+        ),
+        color = TextSecondary
+    )
+}
+
+@Composable
+fun ProductImage(
+    state: DetailState,
+    roundBottomEnd : Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(SurfaceHigher)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    BG,
+                    RoundedCornerShape(
+                        topStart = 0.dp,
+                        topEnd = 0.dp,
+                        bottomStart = 0.dp,
+                        bottomEnd = if (roundBottomEnd) 16.dp else 0.dp
                     )
-                    .padding(dimens.paddingHorizontal)
-            ) {
-                AddToCartButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    price = state.cardTotal
+                )
+        ) {
+            state.product?.let {
+                Image(
+                    painter = rememberAsyncImagePainter(Urls.getProductImageUrl(state.product)),
+                    contentDescription = "Icon",
+                    modifier = Modifier
+                        .size(240.dp),
+                    contentScale = ContentScale.Crop
                 )
             }
         }
